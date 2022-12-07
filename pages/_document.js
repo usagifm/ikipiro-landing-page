4c0fcb57f95eff04f8aa8d8d64c910fcbe7bece8
usagifm <usagifm@gmail.com>
2022-12-08 02:54:58 +0700

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head > 
+        <link rel="shortcut icon" href="assets/images/favicon-transparent.png" />
+        </Head>
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
